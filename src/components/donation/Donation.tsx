430453654f95ac7179636d7e8628c62bf02d969b
Nick O'Neill <nick.oneill@gmail.com>
2018-09-05 18:41:04 -0700

    Updated donation bar with new tools button.

--- src/components/donation/Donation.tsx
+++ src/components/donation/Donation.tsx
@@ -18,9 +18,13 @@ export class Donation extends React.Component<Props, State> {
             <p className="logo__header__donatetext">
               Get Involved:
             </p>
-            <div className="logo__header__donatebutton">
+            {/* <div className="logo__header__donatebutton">
               <a href={`${Constants.WRITER_APPLY_URL}`} target="_blank">Apply</a>
               <p>Research and Write Topics</p>
+            </div> */}
+            <div className="logo__header__donatebutton">
+              <a href="https://airtable.com/shrSFvr3AlMKRRssx" target="_blank">Contact Us</a>
+              <p>Get Your Own 5 Calls Page</p>
             </div>
             <div className="logo__header__donatebutton">
               <a href={`${Constants.CODE_PROJECT_URL}`} target="_blank">Projects</a>
@@ -30,10 +34,6 @@ export class Donation extends React.Component<Props, State> {
               <a href={`${Constants.DONATE_URL}?amount=25`} target="_blank">Donate</a>
               <p>Be a 5 Calls Supporter</p>
             </div>
-            {/* <div className="logo__header__donatebutton">
-              <a href={`${Constants.DONATE_URL}?amount=100`} target="_blank">$100</a>
-              <p>Keeps the server running for a month</p>
-            </div> */}
           </div>
         </div>
       </div>
