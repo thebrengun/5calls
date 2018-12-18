14e619cf817aae06f0aa840beaa6ee1c5c64aa79
Nick O'Neill <nick.oneill@gmail.com>
2018-12-18 13:35:34 -0800

    Don't need a new version of testcafe.

--- runner.js
+++ runner.js
@@ -4,9 +4,15 @@ let testcafe = null;
 
 const browsers = [
   //  ['browserstack:edge@17.0:Windows 10'],
-  ['browserstack:chrome@69.0:Windows 10', 'browserstack:firefox@61.0:Windows 10'],
-  ['browserstack:firefox@61.0:OS X High Sierra', 'browserstack:chrome@69.0:OS X High Sierra'],
-  ['browserstack:safari@11.1:OS X High Sierra'],
+  [
+    'browserstack:chrome@69.0:Windows 10',
+    'browserstack:firefox@61.0:Windows 10'
+  ],
+  [
+    'browserstack:firefox@61.0:OS X High Sierra',
+    'browserstack:chrome@69.0:OS X High Sierra'
+  ],
+  ['browserstack:safari@11.1:OS X High Sierra']
 ];
 
 const runTest = browser => {
@@ -26,9 +32,9 @@ const runTest = browser => {
     .then(async failedCount => {
       console.log('Tests failed: ' + failedCount);
       await testcafe.close();
-      return {failedCount};
+      return { failedCount };
     });
-}
+};
 
 const runAllBrowsers = async () => {
   let failedCount = 0;
@@ -36,20 +42,24 @@ const runAllBrowsers = async () => {
     const result = await runTest(browser);
     failedCount += result.failedCount;
   }
-  return {failedCount};
-}
+  return { failedCount };
+};
 
 const runE2ETests = () => {
-  axios.get('http://localhost:3000')
+  axios
+    .get('http://localhost:3000')
     .then(resp => runAllBrowsers())
     .then(result => {
       console.log(`\nTotal Failures: ${result.failedCount}`);
       process.exit(result.failedCount === 0 ? 0 : 2);
     })
     .catch(err => {
-      console.log('Please start the site locally before running tests by running "yarn start"');
+      console.log(
+        'Please start the site locally before running tests by running "yarn start"',
+        err
+      );
       process.exit(1);
     });
-}
+};
 
 runE2ETests();
