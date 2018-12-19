38513972e8291688543439a0cfe168d4d23d0813
Nick O'Neill <nick.oneill@gmail.com>
2018-12-18 17:28:05 -0800

    Only notify bugsnag on production builds.

--- src/services/bugsnag.ts
+++ src/services/bugsnag.ts
@@ -2,7 +2,10 @@ import * as React from 'react';
 import bugsnag from '@bugsnag/js';
 import bugsnagReact from '@bugsnag/plugin-react';
 
-const bugsnagClient = bugsnag('67e3931dbe1bbf48991ce7d682ceb676');
+const bugsnagClient = bugsnag({
+  apiKey: '67e3931dbe1bbf48991ce7d682ceb676',
+  notifyReleaseStages: ['production']
+});
 bugsnagClient.use(bugsnagReact, React);
 
 export default bugsnagClient;
