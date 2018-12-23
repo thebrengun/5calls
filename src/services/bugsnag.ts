c65f614b9f062f7c9c4bf882c4cc053303744504
Nick O'Neill <nick.oneill@gmail.com>
2018-12-23 09:48:33 -0800

    The incorrect way to mock bugsnag.

--- src/services/bugsnag.ts
+++ src/services/bugsnag.ts
@@ -2,10 +2,16 @@ import * as React from 'react';
 import bugsnag from '@bugsnag/js';
 import bugsnagReact from '@bugsnag/plugin-react';
 
-const bugsnagClient = bugsnag({
-  apiKey: '67e3931dbe1bbf48991ce7d682ceb676',
-  notifyReleaseStages: ['production']
-});
-bugsnagClient.use(bugsnagReact, React);
+let bugsnagClient;
+// hi this is really bad but I can't figure out how to mock this everywhere otherwise
+if (process.env.JEST_WORKER_ID) {
+  bugsnagClient = {};
+} else {
+  bugsnagClient = bugsnag({
+    apiKey: '67e3931dbe1bbf48991ce7d682ceb676',
+    notifyReleaseStages: ['production']
+  });
+  bugsnagClient.use(bugsnagReact, React);
+}
 
 export default bugsnagClient;
