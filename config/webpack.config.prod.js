85f299739fdc96cf28aa25f6542fef5f85920ceb
Nick O'Neill <nick.oneill@gmail.com>
2019-02-22 14:32:25 -0800

    Does this need the css post-loader

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -129,6 +129,24 @@ module.exports = {
               },
               sourceMap: true
             }
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9' // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009'
+                })
+              ]
+            }
           }
         ]
       },
